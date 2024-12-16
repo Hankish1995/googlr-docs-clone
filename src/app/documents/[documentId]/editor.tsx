95e2d0c56ff8918e429da2ec24419b5b9e762aa3
Harmanpreetsohi <hankishbawa17@gmail.com>
2024-12-17 00:07:43 +0530

    changes aved

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -13,6 +13,10 @@ import Dropcursor from "@tiptap/extension-dropcursor";
 import ImageResize from "tiptap-extension-resize-image";
 import { useEditorStore } from "@/store/use-editor-store";
 import Underline from "@tiptap/extension-underline";
+import FontFamily from "@tiptap/extension-font-family";
+import TextStyle from "@tiptap/extension-text-style";
+import { Color } from "@tiptap/extension-color";
+import Highlight from "@tiptap/extension-highlight";
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -43,8 +47,14 @@ export const Editor = () => {
     },
     extensions: [
       StarterKit,
+      FontFamily,
+      TextStyle,
       TaskList,
       Underline,
+      Color,
+      Highlight.configure({
+        multicolor: true,
+      }),
       TaskItem.configure({
         nested: true,
       }),
